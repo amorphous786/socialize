c47ad5adb6f2904c06b3b0a884b0ad8cd41e265f
Usama Nadeem <usamanadeem0786@gmail.com>
2024-03-25 17:46:50 +0500

    registration form

--- account/urls.py
+++ account/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
      # path('password-reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
      # path('password-reset/complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
      path('',include('django.contrib.auth.urls')),
-     path('',views.dashboard,name='dashboard')
+     path('',views.dashboard,name='dashboard'),
+     path('register/',views.register,name='register')
 ]
