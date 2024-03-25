27419a85e25d5d8fcfd0bb4e87b54b74ba6b0e9d
Usama Nadeem <usamanadeem0786@gmail.com>
2024-03-25 18:14:29 +0500

    enough for the day?

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
 from .forms import LoginForm,UserRegistrationForm
 from django.contrib.auth.decorators import login_required
-
+from .models import Profile
 def register(request):
   if request.method=='POST':
     user_form = UserRegistrationForm(request.POST)
@@ -11,6 +11,7 @@ def register(request):
       new_user = user_form.save(commit=False)
       new_user.set_password(user_form.cleaned_data['password'])
       new_user.save()
+      Profile.objects.create(user=new_user)
       return render(request,'account/register_done.html',{'new_user':new_user})
   else:
     user_form = UserRegistrationForm()
