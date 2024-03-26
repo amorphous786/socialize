275f23f1cc44d83d3e7f1811a66b4194a050b76c
Usama Nadeem <usamanadeem0786@gmail.com>
2024-03-26 14:35:58 +0500

    prasdf

--- account/views.py
+++ account/views.py
@@ -41,6 +41,7 @@ def user_login(request):
   
 @login_required 
 def dashboard(request):
+  
   return render(request,
                 'account/dashboard.html',
                 {'section':'dashboard'})
